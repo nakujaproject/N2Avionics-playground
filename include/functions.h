9ed874ba574c413c0f18a92ec63ccb2f0d6f8369
Bange boss <bangegeorgeogeto@gmail.com>
2022-03-23 11:30:45 +0300

    test data points

--- include/functions.h
+++ include/functions.h
@@ -1,24 +1,32 @@
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
 
-
-#include "variables.h" // header file containing variables
 #include "defs.h" // header file containing constants
+#include <WiFi.h>
 
-void createAccessPoint(){
+const char *ssid = "ground-station";
+const char *key = "password";
+
+void createAccessPoint()
+{
   // Connect to Wi-Fi network with SSID and password
   Serial.print("Setting Ground Station WiFi...");
+
   delay(1000);
 
   // Remove the password parameter, if you want the AP (Access Point) to be open
   WiFi.softAP(ssid, key);
 
   IPAddress IP = WiFi.softAPIP();
-  Serial.print("ssid:"); Serial.print(ssid); Serial.println();
-  Serial.print("key:"); Serial.print(key); Serial.println();
-  Serial.print("Rocket IP address: "); Serial.print(IP); Serial.println();
-  
-  server.begin();
+  Serial.print("ssid:");
+  Serial.print(ssid);
+  Serial.println();
+  Serial.print("key:");
+  Serial.print(key);
+  Serial.println();
+  Serial.print("Rocket IP address: ");
+  Serial.print(IP);
+  Serial.println();
 }
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,11 @@
 /*
  * Check brownout issues to prevent ESP32 from re-booting unexpectedly
  */
-// #include "../include/functions.h"
+
+#include "soc/soc.h"
+#include "soc/rtc_cntl_reg.h"
+
+#include "../include/functions.h"
 #include "../include/readsensors.h"
 #include "../include/kalmanfilter.h"
 #include "../include/checkState.h"
@@ -43,7 +47,11 @@ int counter = 0;
 int state = 0;
 void setup()
 {
-  init_components();
+  // init_components();
+  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0);
+  Serial.begin(BAUD_RATE);
+  Serial.println("Bange is boss");
+  createAccessPoint();
 }
 
 void loop()
@@ -54,4 +62,22 @@ void loop()
   filtered_values = kalmanUpdate(readings.altitude, readings.az);
   state = checkState(filtered_values.displacement, filtered_values.velocity, counter, state);
   counter = counter + 1;
+
+  struct LogData ld;
+  ld.counter = counter;
+  ld.altitude = 1.2;
+  ld.ax = 1.2;
+  ld.ay = 1.2;
+  ld.az = 1.2;
+  ld.gx = 5.2;
+  ld.gy = 3.6;
+  ld.gz = 69.2;
+  ld.filtered_s = 1.2;
+  ld.filtered_v = 1.2;
+  ld.filtered_a = 1.2;
+  ld.states = 3;
+  ld.longitude = 2.4;
+  ld.latitude = 3.2;
+
+  serveData(ld);
 }
